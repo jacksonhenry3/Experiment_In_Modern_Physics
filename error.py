6d98d073cd94c06f46f6a7f766f364f04ee8c36d
Jackson Henry <jacksonhenry3@gmail.com>
2014-09-13 12:29:07 -0400

    commented error calculation

--- error.py
+++ error.py
@@ -1,21 +1,26 @@
-from sympy import *
-from sympy.utilities.lambdify import lambdify
-
+from sympy import diff,sqrt #a symbolic math library (Computer Algebra System)
+from sympy.utilities.lambdify import lambdify #allows us to make a python function from a symbolic sympy one
 
 def getError(variables,errorVariables,func):
-	FullErrorFunc = 0
-	pieces = []
-	for i in range(len(variables)):
-		v = variables[i]
-		dv = errorVariables[i]
-		D = (diff(func,v)*dv)**2
-		pieces.append(D)
-	for piece in pieces:
-		FullErrorFunc+=piece
-	FullErrorFunc = sqrt(FullErrorFunc)
-	variables.extend(errorVariables)
-	func = lambdify(tuple(variables), FullErrorFunc ,"numpy") 
-	# print(FullErrorFunc)
+	"""getError generates a function to calculate the error of a function. I.E. a function that
+	gives you the error in its result given the error in its input. Output function is numpy ready.
+
+	arguments
+	variables      : a list of sympy symbols in func
+	errorVariables : list of sympy ymbols representing the error in each value of variables. must be the same length as variables
+	func           : a function containing your variables that you want the error of"""
+	ErrorFunc = 0 # need to set function to start value
+	pieces        = [] #this will later contain the seperate error from each var in func
+	for i in range(len(variables)): #run through all variables in the function
+		v         = variables[i]
+		dv        = errorVariables[i]
+		D         = (diff(func,v)*dv)**2
+		ErrorFunc += D
+
+	variables.extend(errorVariables) #create a list of all sympy symbols involved
+	ErrorFunc = sqrt(ErrorFunc) #this is the final formula for error
+	# print(ErrorFunc) #uncomment this line to see what the error function is
+	func      = lambdify(tuple(variables), ErrorFunc ,"numpy") #convert ErrorFunc to a numpy rteady python lambda
 	return(func)
 
 
--- errorExample.py
+++ errorExample.py
@@ -1,16 +1,26 @@
-import error as er
-import numpy as np
+import error as er #get the error library
+import numpy as np #get numpy
+from sympy import Symbol,pi,sqrt #import required symbolic math components
 
-from sympy import Symbol,pi
+# initialize symbols
 x    = Symbol('x')
 y    = Symbol('y')
 dx   = Symbol('dx')
 dy   = Symbol('dy')
-f    = 4*pi**2/x+y**4
 
+# function to calculate error of
+f    = sqrt(4*pi**2/x+y**4)
+
+# get the error function
 func =  er.getError([x,y],[dx,dy],f)
+
+# measured values of variables and there errors
 x    = np.array([.0404,.05])
 dx   = np.array([.00031,.0004])
 y = np.array([1,2])
 dy = np.array([.1,.01])
-print(func(x,y,dx,dy))
+
+# calculate error for each point
+error = func(x,y,dx,dy)
+
+print error
