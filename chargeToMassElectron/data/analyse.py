59f9faca75e7ccc30b671237151e6abd547d69fa
Jackson Henry <jacksonhenry3@gmail.com>
2014-09-19 13:14:09 -0400

    added e/me data

---
+++ chargeToMassElectron/data/analyse.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import sys
+sys.path.insert(0, '../../')
+
+import error as er #get the error library
+from sympy import Symbol,sqrt #import required symbolic math components
+
+data  = np.loadtxt('data.txt',skiprows = 1)
+voltage = data[:,0]
+current= data[:,1]
+diameter = data[:,2]
+diameterErr = np.ones(len(diameter))*.4
+voltageErr = np.ones(len(voltage))*.5
+voltageErr[-1] = 5
+voltageErr[-2] = 5
+voltageErr[-3] = 5
+voltageErr[-4] = 5
+
+currentError = np.ones(len(current))*.5
+rootVByI = np.sqrt(voltage)/current
+rootVByIErr = np.sqrt(voltageErr**2/(4*current**2*voltage)+voltage*currentError**2/(current**4))
+
+# a      =  np.expand_dims(,axis = 1)
+dat = np.transpose(np.array([voltage,voltageErr,current,currentError,diameter,diameterErr,rootVByI,rootVByIErr]))
+np.savetxt('finalColumnData.txt',dat,fmt = '%.2e' )
+
+
